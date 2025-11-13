fe030a16dbe644da31611c0fef9518c4e7ff5c62
DrStang <48525932+DrStang@users.noreply.github.com>
2025-11-13 14:25:13 -0500

    Update emailService.js

--- emailService.js
+++ emailService.js
@@ -13,7 +13,9 @@ class EmailService {
     }
 
     this.transporter = nodemailer.createTransporter({
-      service: process.env.EMAIL_SERVICE || 'gmail',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT || 587,
+      secure: process.env.EMAIL_SECURE || false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
